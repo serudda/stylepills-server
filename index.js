f8401684ec72b638d63ecfe2a8605f4db9442585
sergioruizdavila <sergioruizdavila@gmail.com>
2017-09-17 01:22:12 -0500

    Add: Created index.js in order to fix error on debug VS Code

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+/* It was neccesary to created this file in order to fix error on debug with VS Code
+It seems that we fixed the issue added: babel-register module 
+reference: https://github.com/katopz/vscode-debug-nodejs-es6
+*/
 require('babel-register');
 require('./server.js');
