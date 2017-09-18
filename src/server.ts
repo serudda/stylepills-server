eeb01ff0ecc7560a35f165eb85b8058a565e9ebe
sergioruizdavila <sergioruizdavila@gmail.com>
2017-09-17 20:30:25 -0500

    Mod: Refactor server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
+/************************************/
+/*           DEPENDENCIES           */
+/************************************/
 import * as express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import * as bodyParser from 'body-parser';
 import schema from './data/schema';
 
+// CONSTANTS
 const GRAPHQL_PORT = 3000;
+const GRAPHQL_ROUTE = '/graphql';
+const GRAPHIQL_ROUTE = '/graphiql';
 
 const graphQLServer = express();
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// INIT GRAPHQL SERVER
+graphQLServer.use(GRAPHQL_ROUTE, bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use(GRAPHIQL_ROUTE, graphiqlExpress({ endpointURL: GRAPHQL_ROUTE }));
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
