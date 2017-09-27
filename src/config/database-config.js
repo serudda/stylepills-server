afcc660275c58d25b6f592e49d0881f097f7ea27
sergioruizdavila <sergioruizdavila@gmail.com>
2017-09-26 20:38:39 -0500

    Add: Added Sequelize to run in Heroku

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -15,7 +15,7 @@ Buscar una solucion para sea el caso que sea siempre tome este archivo.*/
 exports.databaseConfig = {
     username: 'sergioruizdavila',
     password: 'admin',
-    database: 'stylepills_dev',
+    database: 'stylepills_db',
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -27,7 +27,7 @@ Buscar una solucion para sea el caso que sea siempre tome este archivo.*/
 export const databaseConfig: IDatabaseConfig = {
     username: 'sergioruizdavila',
     password: 'admin',
-    database: 'stylepills_dev',
+    database: 'stylepills_db',
     host: '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
